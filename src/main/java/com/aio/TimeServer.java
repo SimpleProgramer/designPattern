373b4914bd7ce6596973629142366c2ea11f6ab0
wangzun <1125334796@qq.com>
2019-02-28 15:50:53 +0800

    修改aio server启动

--- src/main/java/com/aio/TimeServer.java
+++ src/main/java/com/aio/TimeServer.java
@@ -10,5 +10,6 @@ public class TimeServer {
     public static void main(String[] args) {
         int port = 8080;
         AsyncTimeServerHandler timeServer = new AsyncTimeServerHandler(port);
+        new Thread(timeServer, "AIOTimeServerHandler-001").start();
     }
 }
